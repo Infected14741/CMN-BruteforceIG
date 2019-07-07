5db8b11e6010286273f43913874bc2ceb82c00da
Infected_AOFSFOF <42504662+Infected14741@users.noreply.github.com>
2019-07-07 10:23:23 +0700

    Create Restart.py

---
+++ Restart.py
@@ -0,0 +1,26 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('Loading...')
+mengetik('Mohon Tunggu...')
+mengetik('Meminta Akses...')
+mengetik('Memeriksa File...')
+mengetik('Cek STORAGE...')
+mengetik('Cek SDCARD')
+mengetik('Mengambil IMAGES...')
+mengetik('SAVE IMAGES Succes...')
+mengetik('Meminta Akses...')
+mengetik('Login Success...')
+mengetik('WARNING...!!!')
+mengetik('Jika Anda Recode SC Saya...')
+mengetik('Atau CopyRight SC Saya...')
+mengetik('Siap - Siap Saya Tempel Foto ANDA...')
+mengetik('ke Hasil WEBSITE Yang Saya Retas...!!!')
+mengetik('DAN Anda Akan Jadi Buronan Internasional...!!!')
+mengetik('Saya Infected_AOFSFOF...')
+mengetik('Mengucapkan Selamat Memakai SC Saya...!!!')
